a9d7e8d760762dbf3dd7251b7cdfcd3ac233cce4
joaopauloss <jpaulo.em@gmail.com>
2021-11-07 22:55:10 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,31 @@
+import { Seller } from "./sellers"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[]; // ? = valor opcional na hora de preencher o estado
+    last?: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    // "sort": {
+    //     "empty": false,
+    //     "unsorted": false,
+    //     "sorted": true
+    // },
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
