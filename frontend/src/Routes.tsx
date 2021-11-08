f529059f10ff4fdc56d5b74c63642d5e98792b70
joaopauloss <jpaulo.em@gmail.com>
2021-11-07 21:40:00 -0300

    First request

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,18 +3,18 @@ import Home from 'pages/Home';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/dashboard">
-                    <Dashboard />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/dashboard">
+					<Dashboard />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default Routes;
